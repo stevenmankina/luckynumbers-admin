04c550405486770b04e489beaccf16719fc63265
Pranit <pranit@webxstudio.in>
2022-11-25 00:57:13 +0530

    complete

--- src/components/Step2.jsx
+++ src/components/Step2.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Step1 = ({ setStep }) => {
+  const [numbers, setNumbers] = useState({ num: [] });
+
+  const handleAddNo = (e) => {
+    let newNums = numbers.num;
+    let numToAdd = e.target.value;
+
+    if (!newNums.includes(numToAdd)) {
+      newNums.push(numToAdd);
+      setNumbers({
+        ...numbers,
+        num: newNums,
+      });
+    }
+  };
+
   return (
     <>
       <div className="p-10">
@@ -14,6 +29,51 @@ const Step1 = ({ setStep }) => {
         >
           CONTINUE &gt;{" "}
         </button>
+
+        <div className="flex justify-around">
+          <div className="flex ">
+            <p className="px-3 py-1">Add Number</p>
+
+            <div className="flex-col flex">
+              {numbers &&
+                numbers.num.map((num) => (
+                  <p className="py-1 px-3 border-2 rounded-md" key={num}>
+                    {num}
+                  </p>
+                ))}
+
+              <select
+                name="addno"
+                onChange={(e) => handleAddNo(e)}
+                className=" py-1 px-2 my-1 border-2 rounded-md border-primary-300 outline-none"
+                id=""
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+          </div>
+          <div className="flex ">
+              <p className="px-3">Add Time</p>
+            <div className="">
+
+              <input
+                type="time"
+                className="outline-none border-2 px-1 rounded-md border-primary-300"
+                name="date"
+                step={1}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/Step3.jsx
+++ src/components/Step3.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
-const Step1 = ({setStep}) => {
+const Step1 = ({ setStep }) => {
   return (
-<>
-<div className="p-10">
+    <>
+      <div className="p-10">
         <h3 className="text-xl text-primary-500">
           Step 3 - Add number of winning cards per round
         </h3>
@@ -14,10 +14,105 @@ const Step1 = ({setStep}) => {
         >
           CONTINUE &gt;{" "}
         </button>
+
+        <div className="flex justify-evenly">
+          <div className="card text-left w-1/4 p-8 rounded-md shadow-md border">
+            <p>
+              <b>Round 1</b> predetermined winning cards - <b>Corners</b>
+            </p>
+
+            <select
+              className=" py-1 w-full my-1 border-2 rounded-md border-primary-300 outline-none"
+              id=""
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+
+            <p className="text-xs leading-5">
+              Lorem ipsum dolor sit, amet consectetur inventore id dolorum sequi
+              sapiente tempore adipisicing elit. Dicta, adipisci blanditiis.
+              Totam animi inventore id dolorum sequi sapiente tempore dolor!
+            </p>
+
+            <button className="px-6 bg-primary-500 py-1 my-3 rounded-md text-white">
+              Save
+            </button>
+          </div>
+          <div className="card text-left w-1/4 p-8 rounded-md shadow-md border">
+            <p>
+              <b>Round 2</b> predetermined winning cards - <b>Middle</b>
+            </p>
+
+            <select
+              className=" py-1 w-full my-1 border-2 rounded-md border-primary-300 outline-none"
+              id=""
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+
+            <p className="text-xs leading-5">
+              Lorem ipsum dolor sit, amet consectetur inventore id dolorum sequi
+              sapiente tempore adipisicing elit. Dicta, adipisci blanditiis.
+              Totam animi inventore id dolorum sequi sapiente tempore dolor!
+            </p>
+
+            <button className="px-6 bg-primary-500 py-1 my-3 rounded-md text-white">
+              Save
+            </button>
+          </div>
+          <div className="card text-left w-1/4 p-8 rounded-md shadow-md border">
+            <p>
+              <b>Round 3</b> predetermined winning cards - <b>Full house</b>
+            </p>
+
+            <select
+              className=" py-1 w-full my-1 border-2 rounded-md border-primary-300 outline-none"
+              id=""
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+
+            <p className="text-xs leading-5">
+              Lorem ipsum dolor sit, amet consectetur inventore id dolorum sequi
+              sapiente tempore adipisicing elit. Dicta, adipisci blanditiis.
+              Totam animi inventore id dolorum sequi sapiente tempore dolor!
+            </p>
+
+            <button className="px-6 bg-primary-500 py-1 my-3 rounded-md text-white">
+              Save
+            </button>
+          </div>
+        </div>
       </div>
+    </>
+  );
+};
 
-</>
-  )
-}
-
-export default Step1
+export default Step1;
